788a6d633a5c384dd771338656f5cba0fd890d7b
Abilash-devops <abilashdevopsaws@gmail.com>
2023-08-19 16:26:55 +0530

    commited

---
+++ 04_commandsub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "what is the date"
+echo "wanted to know date in the number format"
+d=$(date +%F:%H:%M:%S)
+echo "**************Today's date****************"
+echo " Todays date is : $d"
+echo "******************************************"
