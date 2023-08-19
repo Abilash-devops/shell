0ca40f78f99e7056e2d63fb51c6b9d8ac39ffc55
Abilash-devops <abilashdevopsaws@gmail.com>
2023-08-19 16:31:49 +0530

    commited

--- 04_commandsub.sh
+++ 04_commandsub.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+LOGFILE=/root/shell/logs
+SCRIPT_NAME=$0
+LOGPATH=$LOGFILE/$SCRIPT_NAME-log
 echo "what is the date"
 echo "wanted to know date in the number format"
 d=$(date +%F:%H:%M:%S)
 echo "**************Today's date****************"
-echo " Todays date is : $d"
+echo " Todays date is : $d" &>> $LOGPATH
 echo "******************************************"
