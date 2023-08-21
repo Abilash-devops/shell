1d3dd945d25c34106692c2300964c83cb5955d25
Abilash-devops <abilashdevopsaws@gmail.com>
2023-08-21 09:44:37 +0530

    commited

--- 08_condition.sh
+++ 08_condition.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+D=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOG_PATH=/home/ec2-user/shell/logs
+LOGFILE=$LOG_PATH/$SCRIPT_NAME-log
 u=$(id -u)
 if [ $u -ne 0 ]
 then
 echo " Please run the script with root or superuser previliges "
 exit 1
 fi
-yum install git -y
+yum install git -y &>> $LOGFILE
