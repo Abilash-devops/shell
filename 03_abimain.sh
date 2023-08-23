fce41ac3c0f8352d438501d8a8a252015007955a
Abilash-devops <abilashdevopsaws@gmail.com>
2023-08-23 22:32:47 +0530

    commited

--- 03_abimain.sh
+++ 03_abimain.sh
@@ -4,6 +4,10 @@ LOGPATH=/home/centos/shell/logs
 SCRIPT_NAME=$0
 LOGFILE=$LOGPATH/$0-$d-log
 u=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 if [ $u -ne 0 ]
 then
     echo " Bro you dont have acess with this user Please try as root or sudo acess"
@@ -14,9 +18,9 @@ abi(){
 
     if [ $? -ne 0 ]
     then 
-        echo " $1 installation is failure"
+        echo -e " $2 installation is $R FAILURE $N"
     else
-        echo " $1 installtion is success"
+        echo -e " $2 installtion is $G SUCCESS $N"
     fi
 }
 
