3269aa0338f86cc11d6253fa429762fc5486c6d5
Abilash-devops <abilashdevopsaws@gmail.com>
2023-08-23 21:53:38 +0530

    commited

---
+++ 01_abi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+N1=$1
+N2=$2
+N3=$3
+echo "First person name is $N1"
+echo "Second  person name is $N2"
+echo "Third Person name is $N3"
+echo " My script name is $0"
+echo " Given person names are :$@"
+echo " TOtal number of persons are :$#"
